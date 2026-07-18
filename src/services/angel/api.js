6ad8db1d9ba59b7f8e552868aa357738810dd12a
Bhaskar Reddy <YOUR_GITHUB_EMAIL>
2026-07-18 16:52:14 +0530

    Debug Angel API URL

--- src/services/angel/api.js
+++ src/services/angel/api.js
@@ -2,7 +2,14 @@ import axios from "axios";
 
 const BASE_URL = process.env.ANGEL_BASE_URL;
 
+console.log("BASE_URL =", BASE_URL);
+
 export async function angelLogin(payload, headers) {
+  console.log(
+    "Login URL =",
+    `${BASE_URL}/rest/auth/angelbroking/user/v1/loginByPassword`
+  );
+
   const response = await axios.post(
     `${BASE_URL}/rest/auth/angelbroking/user/v1/loginByPassword`,
     payload,
